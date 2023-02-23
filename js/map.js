c40b7223a93e7e7cbf194335cddec40bc45d4fba
Juan  Hinojosa <juanma.hinojosa97@gmail.com>
2023-02-23 19:23:54 -0300

    first commit

---
+++ js/map.js
@@ -0,0 +1,32 @@
+let http = new XMLHttpRequest();
+
+http.open("get", "grid.json", true);
+
+http.send();
+http.onload = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        let products = JSON.parse(this.responseText);
+
+        let output = "";
+
+        for (let item of products) {
+            output += `
+                <div class="card-grid">
+                        <div class="image-grid">
+                            <img src="${item.image}" alt="" srcset="">
+                        </div>
+    
+                        <div class="title-grid">
+                            <h3>${item.titulo}</h3>
+                        </div>
+    
+                        <div class="parra-grid">
+                            <p>${item.texto}</p>
+                        </div>
+                </div>
+            `;
+        }
+
+        document.querySelector(".grid-content").innerHTML = output;
+    }
+}
